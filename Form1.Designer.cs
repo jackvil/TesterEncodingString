cc1bddc792bf0fdb701b7efe9333624b27e5117e
clavijonet@gmail.com <clavijonet@gmail.com>
2022-08-05 09:44:03 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿
+namespace TesterEncodingString
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.lblLongSource = new System.Windows.Forms.Label();
+            this.lblLongResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtSource
+            // 
+            this.txtSource.Location = new System.Drawing.Point(42, 39);
+            this.txtSource.MaxLength = 32767000;
+            this.txtSource.Multiline = true;
+            this.txtSource.Name = "txtSource";
+            this.txtSource.Size = new System.Drawing.Size(660, 126);
+            this.txtSource.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(42, 187);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(184, 28);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Enconding ASCII";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(42, 275);
+            this.txtResult.MaxLength = 32767000;
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(660, 126);
+            this.txtResult.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(258, 187);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(224, 28);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Enconding Special Chars";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(42, 221);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(184, 28);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Enconding UTF 8";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(258, 221);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(224, 28);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Enconding NonprintableChars";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // lblLongSource
+            // 
+            this.lblLongSource.AutoSize = true;
+            this.lblLongSource.Location = new System.Drawing.Point(588, 187);
+            this.lblLongSource.Name = "lblLongSource";
+            this.lblLongSource.Size = new System.Drawing.Size(16, 17);
+            this.lblLongSource.TabIndex = 6;
+            this.lblLongSource.Text = "0";
+            // 
+            // lblLongResult
+            // 
+            this.lblLongResult.AutoSize = true;
+            this.lblLongResult.Location = new System.Drawing.Point(588, 415);
+            this.lblLongResult.Name = "lblLongResult";
+            this.lblLongResult.Size = new System.Drawing.Size(16, 17);
+            this.lblLongResult.TabIndex = 7;
+            this.lblLongResult.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblLongResult);
+            this.Controls.Add(this.lblLongSource);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtSource);
+            this.Name = "Form1";
+            this.Text = "Text Encoding";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtSource;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label lblLongSource;
+        private System.Windows.Forms.Label lblLongResult;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TesterEncodingString
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtResult.Text = EncodingText(txtSource.Text);
+            lblLongSource.Text = $"{txtSource.Text.Length} chars";
+            lblLongResult.Text = $"{txtResult.Text.Length} chars";
+        }
+
+        public string EncodingText(string source)
+        {
+            var resConvert = Encoding.Convert(
+                Encoding.UTF8,
+                Encoding.GetEncoding(
+                    Encoding.ASCII.EncodingName,
+                    new EncoderReplacementFallback(" "),
+                    new DecoderExceptionFallback()
+                    ),
+                Encoding.UTF8.GetBytes(source));
+            string strResult = Encoding.ASCII.GetString(resConvert);
+
+            return strResult;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtResult.Text = EncodingTextSpecialChars(txtSource.Text);
+            lblLongSource.Text = $"{txtSource.Text.Length} chars";
+            lblLongResult.Text = $"{txtResult.Text.Length} chars";
+        }
+
+        public string EncodingTextSpecialChars(string source)
+        {
+
+            //source = Regex.Replace(source, "[éèëêð]", "e");
+            //source = Regex.Replace(source, "[ÉÈËÊ]", "E");
+            //source = Regex.Replace(source, "[àâä]", "a");
+            //source = Regex.Replace(source, "[ÀÁÂÃÄÅ]", "A");
+            //source = Regex.Replace(source, "[àáâãäå]", "a");
+            //source = Regex.Replace(source, "[ÙÚÛÜ]", "U");
+            //source = Regex.Replace(source, "[ùúûüµ]", "u");
+            //source = Regex.Replace(source, "[òóôõöø]", "o");
+            //source = Regex.Replace(source, "[ÒÓÔÕÖØ]", "O");
+            //source = Regex.Replace(source, "[ìíîï]", "i");
+            //source = Regex.Replace(source, "[ÌÍÎÏ]", "I");
+            source = Regex.Replace(source, "[š]", "s");
+            source = Regex.Replace(source, "[Š]", "S");
+            source = Regex.Replace(source, "[ñ]", "n");
+            source = Regex.Replace(source, "[Ñ]", "N");
+            source = Regex.Replace(source, "[ç]", "c");
+            source = Regex.Replace(source, "[Ç]", "C");
+            source = Regex.Replace(source, "[ÿ]", "y");
+            source = Regex.Replace(source, "[Ÿ]", "Y");
+            source = Regex.Replace(source, "[ž]", "z");
+            source = Regex.Replace(source, "[Ž]", "Z");
+            source = Regex.Replace(source, "[Ð]", "D");
+            source = Regex.Replace(source, "[œ]", "oe");
+            source = Regex.Replace(source, "[Œ]", "Oe");
+            source = Regex.Replace(source, "[«»\u201C\u201D\u201E\u201F\u2033\u2036]", "\"");
+            source = Regex.Replace(source, "[\u2026]", "...");
+            source = source.Replace("<", string.Empty);
+            source = source.Replace(">", string.Empty);
+            source = source.Replace("´", string.Empty);
+            source = source.Replace("`", string.Empty);
+            source = source.Replace("^", string.Empty);
+            source = source.Replace("°", string.Empty);
+            source = source.Replace("/", string.Empty);
+            source = source.Replace("|", string.Empty);
+            source = source.Replace("\u00021", string.Empty);
+            source = source.Replace("\x02", " ");
+            source = source.Replace("\x03", " ");
+            return source;
+        }
+
+        public string EncodingTextUTF8(string source)
+        {
+            Encoding enc = new UTF8Encoding(true, true);
+            string strResult = string.Empty;
+            try
+            {
+                byte[] resConvert = enc.GetBytes(source);
+                strResult = Encoding.ASCII.GetString(resConvert);
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return strResult;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            txtResult.Text = EncodingTextUTF8(txtSource.Text);
+            lblLongSource.Text = $"{txtSource.Text.Length} chars";
+            lblLongResult.Text = $"{txtResult.Text.Length} chars";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            txtResult.Text = EncodingNonPrintableChars(txtSource.Text);
+            lblLongSource.Text = $"{txtSource.Text.Length} chars";
+            lblLongResult.Text = $"{txtResult.Text.Length} chars";
+        }
+
+        public string EncodingNonPrintableChars(string source)
+        {
+            return Regex.Replace(source, @"\p{C}+", String.Empty);
+        }
+    }
+}
